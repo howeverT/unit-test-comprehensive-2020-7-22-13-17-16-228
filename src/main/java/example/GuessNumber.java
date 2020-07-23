3d966355994a517e209662badb3eee6a6647d9af
HoweverT <1413537501@qq.com>
2020-07-23 19:51:55 +0800

    [unit test][david] feature: pass should_return_4A0B_when_guess_given_answer1234_and_input_guess_1234

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -2,6 +2,16 @@ package example;
 
 public class GuessNumber {
     public String guess(int[] inputGuess, int[] answer) {
+        if (isCurrent(inputGuess,answer))
+            return "4A0B";
+
         return null;
+    }
+    public boolean isCurrent(int[] inputGuess, int[] answer){
+        for (int index = 0; index < inputGuess.length; index++) {
+            if (inputGuess[index]!=answer[index])
+                return false;
+        }
+        return true;
     }
 }
