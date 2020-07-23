fc6ce32896831b26716c8c7c90bb751aa142f9fd
HoweverT <1413537501@qq.com>
2020-07-23 21:55:48 +0800

    [unit test][david] feature: pass should_return_true_when_generate_answer_given_answer_random

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -59,6 +59,22 @@ public class GuessNumber implements AnswerGenerator{
 
     @Override
     public int[] generate() {
-        return new int[0];
+        int[] result = new int[4];
+        int count = 0;
+        while(count < 4) {
+            int num = (int) (Math.random() * 9);
+            boolean isInResult = true;
+            for (int index = 0; index < 4; index++) {
+                if(num == result[index]){
+                    isInResult = false;
+                    break;
+                }
+            }
+            if(isInResult){
+                result[count] = num;
+                count++;
+            }
+        }
+        return result;
     }
 }
